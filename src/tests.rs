e966aba32d712d88811fd61876386d5b0a39f57b
Laurenz Werner <werner.la@protonmail.com>
2023-04-09 23:26:46 +0200

    add test condition to prevent incorrect key length for PFS keys

--- src/tests.rs
+++ src/tests.rs
@@ -30,6 +30,7 @@ fn test_message_encryption() {
 	let pfs_key = rand::thread_rng().gen::<[u8; 32]>().to_vec();
 	let (enc_msg, new_key) = encrypt_msg(pk, sign_sk, pfs_key.clone(), "testing message encryption").unwrap();
 	assert_ne!(pfs_key, new_key);
+	assert_eq!(new_key.len(), 32);
 	let (dec_msg, other_new_key) = decrypt_msg(sk, Some(sign_pk), pfs_key, enc_msg).unwrap();
 	assert_eq!(new_key, other_new_key);
 	assert_eq!(dec_msg, "testing message encryption".to_string());
